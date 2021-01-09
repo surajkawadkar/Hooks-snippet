7535c97a6b43e9a35d040c0d5b1ea587c9c758ec
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-01-09 14:23:01 +0530

    comment added why we need this

---
+++ src/components/ConditionalEffect.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ConditionalEffect() {
+
+    //useffect call after eery single render it may cause performance problem so we use conditional use effect
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ConditionalEffect
